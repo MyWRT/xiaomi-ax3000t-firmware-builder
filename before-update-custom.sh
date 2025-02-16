1bb2a99ecf4951c3f5eb322f6805ce2aaca879bb
Mike Evans <etnperlong@gmail.com>
2025-02-16 15:04:51 +0000

    package: add luci-app-wolplus, and cleanup

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -12,15 +12,10 @@
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 
 # Add luci-app-netdata
-git clone --depth 1 --branch master --single-branch --no-checkout https://github.com/muink/luci-app-netdata.git package/luci-app-netdata
-pushd package/luci-app-netdata
-umask 022
-git checkout
-popd
+git clone --depth 1 https://github.com/muink/luci-app-netdata.git package/luci-app-netdata
 
 # Add luci-app-wechatpush
-git clone --depth 1 --branch master --single-branch --no-checkout https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
-pushd package/luci-app-wechatpush
-umask 022
-git checkout
-popd
+git clone --depth 1 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
+
+# Add luci-app-wolplus
+git clone --depth 1 https://github.com/animegasan/luci-app-wolplus.git package/luci-app-wolplus
